0efb7ee8c2ffde6bd583326337ac37a503a921d8
Shadwal Sinha <shadwal.sinha@globallogic.com>
2023-09-24 13:11:05 +0530

    Second Commit

--- App.js
+++ App.js
@@ -1,4 +1,15 @@
-const parent= React.createElement("div", {}, React.createElement("div", {id: "child"}, [React.createElement("h1", {},"I'm H1 Tag"),React.createElement("h2", {},"I'm H2 Tag")]));
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+const parent= React.createElement("div", {id:"parent"},[ 
+React.createElement("div", {id: "child"}, [
+    React.createElement("h1", {},"I'm H1 Tag"),
+    React.createElement("h2", {},"I'm H2 Tag"),
+]),
+React.createElement("div",{id:"child2"},[
+    React.createElement("h1",{},"I'm an h1 Tag"),
+    React.createElement("h2",{},"I'm an h2 Tag")
+])
+]);
 
 const root= ReactDOM.createRoot(document.getElementById("root"))
 root.render(parent);
